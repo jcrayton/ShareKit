2753e0ae108b0e69b155e118131b5bd89b7552fc
arifzamil <azamil@macalester.edu>
2018-11-10 03:45:19 -0600

    Adding nav bars

--- src/pages/mission/mission.ts
+++ src/pages/mission/mission.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MissionPage } from '../mission/mission';
+import { BasicKit } from '../basic-kit/basic-kit';
+import { FrequencyPage } from '../frequency/frequency';
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-mission',
@@ -9,6 +13,18 @@ export class MissionPage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  loadMission() {
+    this.navCtrl.push(MissionPage)
+  }
+
+  loadBasicKit() {
+    this.navCtrl.push(BasicKit)
+  }
+
+  loadHome() {
+    this.navCtrl.push(HomePage)
   }
 
 }
