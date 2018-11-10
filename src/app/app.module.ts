b48f0abff80e5049e1f69c7c13ed9602b4d3f2cc
jcrayton <jcrayton@macalester.edu>
2018-11-09 22:26:09 -0600

    added a base for the missionpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { MissionPage } from '../pages/mission/mission';
+
 
 @NgModule({
   declarations: [
